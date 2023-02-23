a266cd5ea3e7b0b1e2d6cd6a1f35c51c01b794d3
possiblemeatball <mwalker@possiblemeatball.net>
2023-02-23 13:56:08 -0500

    actual fix

--- lib/FlexUnlimited.py
+++ lib/FlexUnlimited.py
@@ -68,6 +68,7 @@ class FlexUnlimited:
         self.arrivalBuffer = config["arrivalBuffer"]  # arrival buffer in minutes
         self.desiredStartTime = config["desiredStartTime"]  # start time in military time
         self.desiredEndTime = config["desiredEndTime"]  # end time in military time
+        self.desiredWeekdays = set()
         desiredWeekdays = config["desiredWeekdays"]
         self.retryLimit = config["retryLimit"]  # number of jobs retrieval requests to perform
         self.refreshInterval = config["refreshInterval"]  # sets delay in between getOffers requests
