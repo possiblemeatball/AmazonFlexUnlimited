059cce5191c92fd6432d496bba0d2fc5a94f1712
possiblemeatball <mwalker@possiblemeatball.net>
2023-03-16 11:18:58 -0400

    reformat

--- lib/Offer.py
+++ lib/Offer.py
@@ -8,8 +8,8 @@ class Offer:
 
     def __init__(self, offerResponseObject: object) -> None:
         self.id = offerResponseObject.get("offerId")
-        self.hidden = offerResponseObject.get("hidden")
         self.serviceAreaId = offerResponseObject.get('serviceAreaId')
+        self.hidden = offerResponseObject.get("hidden")
         self.expirationDate = datetime.fromtimestamp(offerResponseObject.get("expirationDate"))
         self.startTime = datetime.fromtimestamp(offerResponseObject.get("startTime"))
         self.endTime = datetime.fromtimestamp(offerResponseObject.get('endTime'))
@@ -25,7 +25,7 @@ class Offer:
     def __str__(self) -> str:
         dict_copy = self.__dict__.copy()
         dict_copy.pop('id')
-        return json.dumps(dict_copy, default=str)
+        return f"Offer{json.dumps(dict_copy, default=str)}"
     
     def strPretty(self) -> str:
         body = f'{self.serviceAreaId}\n'
