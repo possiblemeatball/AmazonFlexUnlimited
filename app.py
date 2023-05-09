a629b4d00123f9280b1f16c1689a920e89e12a88
possiblemeatball <mwalker@possiblemeatball.net>
2023-05-09 11:57:02 -0400

    fix prettytable error

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
       print("\n Your service area options:")
       serviceAreasTable = PrettyTable()
       serviceAreasTable.field_names = ["Service Area ID", "Service Area Name"]
-      for serviceArea in flexUnlimited.service_areas_map:
-        serviceAreasTable.add_row(serviceArea)
+      for service_area_key in flexUnlimited.service_areas_map.keys() :
+        serviceAreasTable.add_row(service_area_key, flexUnlimited.service_areas_map[service_area_key])
       print(serviceAreasTable)
     else:
       print("Invalid argument provided.")
